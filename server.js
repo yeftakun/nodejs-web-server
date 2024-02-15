2ef2cfba21d88f6b924efc820bc989606dcf7174
yeftakun <yeftakun34@gmail.com>
2024-02-15 22:51:14 +0800

    Routing Request

--- server.js
+++ server.js
@@ -1,37 +1,4 @@
 const http = require('http');
- 
-// const requestListener = (request, response) => {
-//     response.setHeader('Content-Type', 'text/html');
-//     response.statusCode = 200;
- 
-//     const { method } = request;
- 
-//     if(method === 'GET') {
-//         response.end('<h1>Hello!</h1>');
-//     }
- 
-//     if(method === 'POST') {
-//         let body = []; //sebagai array untuk buffer
-        
-//         request.on('data', (chunk) => {
-//             body.push(chunk);
-//         })
-
-//         request.on('end', () => {
-//             body = Buffer.concat(body).toString();
-//             const { name } = JSON.parse(body);
-//             response.end(`<h1>Hai, ${name}!</h1>`)
-//         })
-//     }
- 
-//     if(method === 'PUT') {
-//         response.end('<h1>Bonjour!</h1>');
-//     }
- 
-//     if(method === 'DELETE') {
-//         response.end('<h1>Nihao, hou a youa!</h1>');
-//     }
-// };
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
